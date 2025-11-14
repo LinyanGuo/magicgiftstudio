e89a85da54a6c9e47347016d0b8b1df87b86f699
Linyan Guo <l27guo@uwaterloo.ca>
2025-11-14 10:23:20 -0500

    chore: safer prisma client + dynamic homepage

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,18 @@
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-	// eslint-disable-next-line no-var
-	var __prisma: PrismaClient | undefined;
-}
+// 在全局声明，避免热重载导致的多个实例
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const prisma = global.__prisma ?? new PrismaClient();
+// 在生产环境使用单例，在开发环境避免重复实例
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'], // 可调试 Prisma 查询
+  });
 
-if (process.env.NODE_ENV !== 'production') global.__prisma = prisma;
+// 只有在开发环境下缓存全局实例（生产环境 Vercel 是无状态，不需要缓存）
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
